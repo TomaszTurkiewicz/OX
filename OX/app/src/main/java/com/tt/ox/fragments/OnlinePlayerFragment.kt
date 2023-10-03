f8084dd79c20665aa7a05fd60df45bd1f30d50e8
tt <tturkiewicz83@gmail.com>
2023-10-03 19:50:20 +0100

    online player logging in first try

--- OX/app/src/main/java/com/tt/ox/fragments/OnlinePlayerFragment.kt
+++ OX/app/src/main/java/com/tt/ox/fragments/OnlinePlayerFragment.kt
@@ -1,25 +1,102 @@
 package com.tt.ox.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import com.tt.ox.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.tt.ox.databinding.FragmentOnlinePlayerBinding
 
 class OnlinePlayerFragment : Fragment() {
 
+    private var _binding: FragmentOnlinePlayerBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var auth:FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("329182313552-1nrhrejp03ndlhnvff60leaj2p87sk5p.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                val data: Intent? = result.data
+                doSomething(data)
+            }
+        }
+    }
+
+    private fun doSomething(data: Intent?) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        try {
+            val account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account.idToken)
+        }catch (e: ApiException){
+            Log.w("TAG","Google sign in failed", e)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken:String?){
+        val credentials = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credentials)
+            .addOnCompleteListener(requireActivity()){ task ->
+                if(task.isSuccessful){
+                    val user = Firebase.auth.currentUser
+                    if(user!=null){
+                        binding.account.text = user.uid
+                        // create user if not exists or compare if exists
+                    }
+                }else{
+                    binding.account.text = "FAIL TO LOG IN"
+                }
+            }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_online_player, container, false)
+        _binding = FragmentOnlinePlayerBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.login.setOnClickListener {
+            val signInIntent = googleSignInClient.signInIntent
+            resultLauncher.launch(signInIntent)
+        }
+
+        binding.logout.setOnClickListener {
+            auth.signOut()
+        }
+    }
 }
+
+/* todo
+*   check if firebase user not null
+*   if null ask for login
+*   if not null display active users
+*   when logging in create database if not created
+*   time stamp and saving by date
+ */
